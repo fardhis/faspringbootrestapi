396af05a1dc4249629f9e01dbaf3f404397f245d
Fadillah Ardhi <f.ardhi.77@gmail.com>
2023-07-22 08:14:29 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/faspringbootrestapi/FaspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.faspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaspringbootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/faspringbootrestapi/FaspringbootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.faspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaspringbootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
