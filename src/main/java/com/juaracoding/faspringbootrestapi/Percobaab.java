5f31e510936b8fda2a0991d4c6960e6b7a17c07e
Fadillah Ardhi <f.ardhi.77@gmail.com>
2023-07-22 08:34:16 +0700

    percobaan

--- src/main/java/com/juaracoding/faspringbootrestapi/Percobaab.java
+++ src/main/java/com/juaracoding/faspringbootrestapi/Percobaab.java
@@ -4,11 +4,11 @@ Created by IntelliJ IDEA 2023.1.2 (Community Edition)
 Build #IC-231.9011.34, built on May 16, 2023
 @Author F_Ardhi a.k.a. Fadillah Ardhi
 Java Developer
-Created on 7/22/2023 8:17 AM
-@Last Modified 7/22/2023 8:17 AM
+Created on 7/22/2023 8:32 AM
+@Last Modified 7/22/2023 8:32 AM
 Version 1.0
 */
 
-public class Percobaan {
+public class Percobaab {
 }
 
